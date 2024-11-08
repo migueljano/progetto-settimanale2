6d1649224d83e4d2a42a8d5e1652e29e920c8680
migueljano <angel1999dtb@gmail.com>
2024-11-08 16:51:23 +0100

    progress-project1

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -11,18 +11,31 @@ REGOLE
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
 const pets = ['dog', 'cat', 'hamster', 'redfish']
-
+ for (let i = 0; i < pets.length; i++) {
+  console.log (pets[i]);
+ }
+  
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
 
+console.log('ordine alfabetico', pets.sort());
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
-
+pets.reverse() ;
+for (let i = 0; i < pets.length; i++) {
+  console.log (pets[i]);
+}
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+const pets1 = ['dog', 'cat', 'hamster', 'redfish', 'gatto'];
+
+
+const firstPet = pets1.shift();  
+pets1.push(firstPet);            
+console.log(pets1);
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
@@ -48,15 +61,26 @@ const cars = [
   },
 ]
 
+for(let i= 0; i < cars.length; i++) {
+  cars[i].licensePlate = 'In+ter25';
+}
+console.log(cars);
+
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
-
+cars.push({ brand: 'mercedes', model: 'classe A', color: 'black', trims: ['life', 'style', 'r-line']});
+console.log('Array con new object:', cars);
+for(let i = 0; i< cars.length; i++) {
+  cars[i].trims.pop();
+}
+console.log('array senza trim:', cars)
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = []
+
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
